4149b79a8638d07933862113c5a13e346685fb87
Charles Dessenius <dessenius.charles@gmail.com>
2024-01-24 22:33:00 +0100

    Screen now horizontal + corrections

--- STONE OF SCENARIO/code.py
+++ STONE OF SCENARIO/code.py
@@ -4,6 +4,7 @@
 #############################################################
 import random
 import board
+from time import sleep
 from analogio import AnalogIn
 from story import story
 from adafruit_pyportal import PyPortal
@@ -11,7 +12,7 @@ from adafruit_pyportal import PyPortal
 #############################################################
 #                          CONTENT                          #
 #############################################################
-# --------------- Colors ----------------- #
+# ---------------- Colors ---------------- #
 WHITE = 0xFFFFFF
 BLACK = 0x000000
 FIRE = 0xAA5500
@@ -19,6 +20,14 @@ EARTH = 0x64EE64
 WATER = 0x0687FF
 ELIXIR = 0x9F272F
 AIR = 0xB0BCBF
+
+# --------------- Captions --------------- #
+CAPTIONS = [["FEU", (32, 300), FIRE],
+            ["TERRE", (120, 300), EARTH],
+            ["EAU", (230, 300), WATER],
+            ["ELIXIR", (320, 300), ELIXIR],
+            ["AIR", (420, 300), AIR],
+            ]
 
 cwd = ("/"+__file__).rsplit('/', 1)[0] # the current working directory (where this file is)
 
@@ -34,8 +43,8 @@ soundNext3 = '/sounds/next3.wav'
 
 # ------------- Screen Setup ------------- #
 display = board.DISPLAY
-display.rotation = 90
-# display.auto_brightness = True
+display.rotation = 0
+display.brightness = 0.4
 
 # ------------ Pyportal Setup ------------ #
 pyportal = PyPortal(
@@ -44,17 +53,17 @@ pyportal = PyPortal(
                     text_font = "fonts/Helvetica-Bold-16.bdf",
                     caption_font = "fonts/Helvetica-Bold-16.bdf",
                     # text_font = "fonts/Arial-Bold-24.bdf",
-                    text_position = (13, 240),
+                    text_position = (13, 150),
                     text_color = WHITE
                    )
 
 #############################################################
 #                         MAIN LOOP                         #
 #############################################################
-pyportal.set_text((" " * 18) + "Chargement...") # display while user waits
+pyportal.set_text((" " * 33) + "Chargement...") # display while user waits
 pyportal.play_file(soundNext2)
 pyportal.preload_font() # speed things up by preloading font
-pyportal.set_text((" "*7) + "La Renaissance Élémentaire\n"  + (" " * 18) + "Un Air Malade\n \n" + (" " * 17) + "Touchez l'ecran\n " + (" " * 17) + "pour continuer") # show title
+pyportal.set_text((" "*23) + "La Renaissance Élémentaire\n"  + (" " * 34) + "Un Air Malade\n \n" + (" " * 33) + "Touchez l'ecran\n " + (" " * 33) + "pour continuer") # show title
 pyportal.play_file(soundNext3)
 
 
@@ -63,21 +72,22 @@ while True:
         pyportal.play_file(soundNext1)
         # get random string from array and wrap w line breaks
         if len(story) > 0 :
-            strat = pyportal.wrap_nicely(story.pop(0), 32)
+            # print(f"story = {len(story)}")
+        
+            strat = pyportal.wrap_nicely(story.pop(0), 54)
             outstring = '\n'.join(strat)
 
             # display new text
             pyportal.set_text(outstring, 0)
         else :
-            pyportal.set_text("Bonne chance, Héros des éléments !", 0)
-            pyportal.set_caption("FEU", (10, 260), FIRE)
-            pyportal.set_caption("TERRE", (60, 260), EARTH)
-            pyportal.set_caption("EAU", (140, 260), WATER)
-            pyportal.set_caption("ELIXIR", (200, 260), ELIXIR)
-            pyportal.set_caption("AIR", (270, 260), AIR)
+            pyportal.set_text(" " * 13 + "Bonne chance, Héros des éléments !", 0)
+            sleep(1)
+
+            for caption in CAPTIONS :
+                pyportal.set_caption(caption[0], caption[1], caption[2])
+                sleep(1)
+            
         
         # don't repeat until a new touch begins
         while pyportal.touchscreen.touch_point:
             continue
-
-
